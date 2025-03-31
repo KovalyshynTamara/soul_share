610a9d0952201b755981c2c19272ffc011c6eabd
Tamara Kovalyshyn <114612575+KovalyshynTamara@users.noreply.github.com>
2025-03-31 15:58:08 +0300

    Update song.rb

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,5 +1,5 @@
 class Song < ApplicationRecord
-  belongs_to :added_by, class_name: "User", foreign_key: 'added_by_id'
+  belongs_to :added_by, class_name: "User", foreign_key: "added_by_id"
   has_many :event_songs
   has_many :events, through: :event_songs
   has_many :favorites
