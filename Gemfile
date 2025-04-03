e5eabee3cad6ec4ddad351480d4c46a1e9578b1b
KovalyshynTamara <kovtomara@gmail.com>
2025-04-03 18:07:38 +0300

    setup devise

--- Gemfile
+++ Gemfile
@@ -14,6 +14,8 @@ gem "puma", ">= 5.0"
 
 gem "tailwindcss-rails", "~> 3.3.1"
 
+gem "devise"
+
 # Use Active Model has_secure_password [https://guides.rubyonrails.org/active_model_basics.html#securepassword]
 # gem "bcrypt", "~> 3.1.7"
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   has_many :songs, foreign_key: "added_by_id"
   has_many :events, foreign_key: :created_by_id, class_name: "Event"
   has_many :favorites
@@ -6,5 +10,5 @@ class User < ApplicationRecord
   has_many :event_participants
   has_many :comments, through: :event_participants
   validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: URI::MailTo::EMAIL_REGEXP }
-  validates :name, presence: true, uniqueness: true, length: { minimum: 3, maximum: 20 }
+  # validates :name, presence: true, uniqueness: true, length: { minimum: 3, maximum: 20 }
 end
