73188a334eacce3f9a061c78423944acdf25d174
KovalyshynTamara <kovtomara@gmail.com>
2025-04-03 18:53:46 +0300

    Refactor RSpec tests

--- app/models/event_participant.rb
+++ app/models/event_participant.rb
@@ -1,9 +1,6 @@
 class EventParticipant < ApplicationRecord
   belongs_to :event
   belongs_to :user
-  has_many :event_songs, dependent: :destroy
-  has_many :comments, dependent: :destroy
-
-  validates :event_id, presence: true
-  validates :user_id, presence: true
+  has_many :event_songs
+  has_many :comments
 end
