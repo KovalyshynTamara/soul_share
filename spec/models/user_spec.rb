d113ea23ae3756855812c3a0166b7ec07398187f
KovalyshynTamara <kovtomara@gmail.com>
2025-04-11 18:51:22 +0300

    CI fix

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -22,7 +22,7 @@ RSpec.describe User, type: :model do
       expect(user).to be_valid
     end
   end
-  
+
   it { is_expected.to validate_presence_of(:name) }
   it { is_expected.to validate_uniqueness_of(:name) }
   it { is_expected.to validate_length_of(:name).is_at_least(3).is_at_most(20) }
