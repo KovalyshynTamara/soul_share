1335a8a2ee7d8251c264d6844afb45d0224548f2
KovalyshynTamara <kovtomara@gmail.com>
2025-05-16 09:53:58 +0300

    admin side

---
+++ app/controllers/event_participants_controller.rb
@@ -0,0 +1,19 @@
+class EventParticipantsController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+    @event = resource
+    current_user.event_participants.create(event: @event)
+    redirect_to @event, notice: 'You have joined the event'
+  end
+
+  def destroy
+    @event = resource
+    current_user.event_participants.find_by(event: @event).destroy
+    redirect_to @event, notice: 'You have left the event'
+  end
+  
+  def resource
+    Event.find(params[:event_id])
+  end
+end
