50ad6a0a0ec55972687c3cf36655d2b4dd5cfd12
KovalyshynTamara <kovtomara@gmail.com>
2025-05-18 00:16:13 +0300

    added tests and view templates

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -41,7 +41,11 @@ class Admin::UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :admin)
+    if current_user.admin?
+      params.require(:user).permit(:name, :email, :admin)
+    else
+      params.require(:user).permit(:name, :email)
+    end
   end
 
   def require_admin
